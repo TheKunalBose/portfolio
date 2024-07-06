fa1892e966d230b0d84d2a5d3877c9742a603e4c
kunal <kunalvon3002@gmail.com>
2024-07-06 20:24:56 +0530

    Bento bug fix

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,48 +1,48 @@
 import { cn } from "@/utils/cn";
-import { BackgroundGradientAnimation } from "./Gradientbg";
+import { BackgroundGradientAnimation } from "./GradientBg";
 
 export const BentoGrid = ({
 className,
 children,
 }: {
-className?: string;
-children?: React.ReactNode;
+      className?: string;
+      children?: React.ReactNode;
 }) => {
-return (
-<div
-className={cn(
-      "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
-      className
-)}
->
-{children}
-</div>
-);
+      return (
+      <div
+            className={cn(
+                  "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-3 gap-4 max-w-7xl mx-auto ",
+                  className
+            )}
+      >
+            {children}
+      </div>
+      );
 };
 
 export const  BentoGridItem = ({
-className,
-title,
-description,
-id,
-img,
-imgclassName,
-titelClassName,
-spareImg,
+      className,
+      title,
+      description,
+      id,
+      img,
+      imgclassName,
+      titelClassName,
+      spareImg,
 }: {
-className?: string;
-title?: string | React.ReactNode;
-description?: string | React.ReactNode;
-header?: React.ReactNode;
-icon?: React.ReactNode;
-id?: number;
-img?: string;
-imgclassName?: string;
-titelClassName?: string;
-spareImg?:string;
+      className?: string;
+      title?: string | React.ReactNode;
+      description?: string | React.ReactNode;
+      header?: React.ReactNode;
+      icon?: React.ReactNode;
+      id?: number;
+      img?: string;
+      imgclassName?: string;
+      titelClassName?: string;
+      spareImg?:string;
 }) => {
 return (
-<div
+      <div
 className={cn(
       "row-span-1 relative rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4",
       className
@@ -69,9 +69,18 @@ style={{
                         justify-center text-white font-bold"/>
                   </BackgroundGradientAnimation>
             )}
+            <div className={cn(
+                  titelClassName, 'group-hover/'
+                  )}>
+                  <div className={cn(
+                        'bento:translate-x-2 transition duration-200',
+                        'relative md:h-full min-h-40 flex flex-col px-5',
+                        'p-5 lg:p-10'
+                  )}></div>
+            </div>
       </div>
 
-<div className="group-hover/bento:translate-x-2 transition duration-200">
+<div className="group-hover/bento:translate-x-2 transition duration-200" />
       
       <div className="font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
       {title}
@@ -80,6 +89,6 @@ style={{
       {description}
       </div>
 </div>
-</div>
+
 );
 };
